903fa6353aa60936bf14e6f41869b081ff18bc37
mrvillage <josef@mrvillage.dev>
2023-09-14 00:39:17 -0400

    Add into

--- src/lib.rs
+++ src/lib.rs
@@ -6,13 +6,17 @@ use leptos::{component, view, IntoAttribute, IntoSignal, IntoView, MaybeSignal};
 #[component]
 pub fn Icon(
     icon: MaybeSignal<Icons>,
-    #[prop(default = MaybeSignal::Static("".into()))] class: MaybeSignal<String>,
-    #[prop(default = MaybeSignal::Static(24))] size: MaybeSignal<u16>,
-    #[prop(default = MaybeSignal::Static(2))] stroke_width: MaybeSignal<u16>,
-    #[prop(default = MaybeSignal::Static("currentColor".into()))] stroke: MaybeSignal<String>,
-    #[prop(default = MaybeSignal::Static("none".into()))] fill: MaybeSignal<String>,
-    #[prop(default = MaybeSignal::Static("round".into()))] stroke_linecap: MaybeSignal<String>,
-    #[prop(default = MaybeSignal::Static("round".into()))] stroke_linejoin: MaybeSignal<String>,
+    #[prop(into, default = MaybeSignal::Static("".into()))] class: MaybeSignal<String>,
+    #[prop(into, default = MaybeSignal::Static(24))] size: MaybeSignal<u16>,
+    #[prop(into, default = MaybeSignal::Static(2))] stroke_width: MaybeSignal<u16>,
+    #[prop(into, default = MaybeSignal::Static("currentColor".into()))] stroke: MaybeSignal<String>,
+    #[prop(into, default = MaybeSignal::Static("none".into()))] fill: MaybeSignal<String>,
+    #[prop(into, default = MaybeSignal::Static("round".into()))] stroke_linecap: MaybeSignal<
+        String,
+    >,
+    #[prop(into, default = MaybeSignal::Static("round".into()))] stroke_linejoin: MaybeSignal<
+        String,
+    >,
 ) -> impl IntoView {
     let icon = icon.into_signal();
     let class = move || format!("icon icon-tabler icon-tabler-{} {}", icon(), class());
